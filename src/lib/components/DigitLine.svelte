09e8ad2800ebdb9e2789b8fc7a83bf6a562e2e3e
KnoblauchPilze <prolific-programmer@tuta.com>
2025-01-27 17:46:04 +0100

    feat: Started to create structure for Qwixx game

--- src/lib/components/DigitLine.svelte
+++ src/lib/components/DigitLine.svelte
@@ -3,24 +3,29 @@
 	import { FlexContainer } from '@totocorpsoftwareinc/frontend-toolkit';
 	import GameCard from './GameCard.svelte';
 	import LockCard from './LockCard.svelte';
+	import type { DigitLine } from '$lib/game/line';
 
 	interface Props {
-		digits: number[];
+		line: DigitLine;
 		color: Color;
 	}
 
-	let { digits, color }: Props = $props();
+	let { line, color }: Props = $props();
+
+	const digits = $derived(line.digits);
+	const locked = $derived(line.locked);
+	const score = $derived(line.score);
 </script>
 
 <FlexContainer vertical={false}>
 	<FlexContainer vertical={false} styling="w-4/5 mr-4">
 		{#each digits as digit}
-			<GameCard text={'' + digit} {color} />
+			<GameCard text={'' + digit.value} {color} />
 		{/each}
 	</FlexContainer>
 
 	<FlexContainer vertical={false} styling="w-1/5 ml-4 space-x-2">
-		<LockCard locked={false} {color}></LockCard>
-		<GameCard text={'0'} {color} />
+		<LockCard {locked} {color}></LockCard>
+		<GameCard text={score.toString()} {color} locked={true} />
 	</FlexContainer>
 </FlexContainer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,31 +2,35 @@
 	import DigitLine from '$lib/components/DigitLine.svelte';
 	import GameCard from '$lib/components/GameCard.svelte';
 	import { Color } from '$lib/enums/color';
+	import { Qwixx } from '$lib/game/qwixx';
 	import { FlexContainer, StyledTitle } from '@totocorpsoftwareinc/frontend-toolkit';
 
-	const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-	const decreasingDigits = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+	const game = new Qwixx();
+
+	function getPenaltyText(ticked: boolean): string {
+		return ticked ? 'X' : '';
+	}
 </script>
 
 <FlexContainer align={'stretch'} styling={'border-2 rounded-xl border-accent'}>
 	<StyledTitle text="Qwixx sheet" />
 	<FlexContainer align={'stretch'}>
-		<DigitLine {digits} color={Color.RED}></DigitLine>
-		<DigitLine {digits} color={Color.YELLOW}></DigitLine>
-		<DigitLine digits={decreasingDigits} color={Color.GREEN}></DigitLine>
-		<DigitLine digits={decreasingDigits} color={Color.BLUE}></DigitLine>
+		<DigitLine line={game.reds} color={Color.RED}></DigitLine>
+		<DigitLine line={game.yellows} color={Color.YELLOW}></DigitLine>
+		<DigitLine line={game.greens} color={Color.GREEN}></DigitLine>
+		<DigitLine line={game.blues} color={Color.BLUE}></DigitLine>
 
 		<FlexContainer vertical={false} align={'end'}>
 			<FlexContainer vertical={false} justify={'center'}>
-				<GameCard text={''} color={Color.NEUTRAL}></GameCard>
-				<GameCard text={''} color={Color.NEUTRAL}></GameCard>
-				<GameCard text={''} color={Color.NEUTRAL}></GameCard>
-				<GameCard text={''} color={Color.NEUTRAL}></GameCard>
+				<GameCard text={getPenaltyText(game.penalties > 0)} color={Color.NEUTRAL}></GameCard>
+				<GameCard text={getPenaltyText(game.penalties > 0)} color={Color.NEUTRAL}></GameCard>
+				<GameCard text={getPenaltyText(game.penalties > 0)} color={Color.NEUTRAL}></GameCard>
+				<GameCard text={getPenaltyText(game.penalties > 0)} color={Color.NEUTRAL}></GameCard>
 				<GameCard text={'0'} color={Color.NEUTRAL} locked={true}></GameCard>
 			</FlexContainer>
 
 			<FlexContainer vertical={false} justify={'center'}>
-				<GameCard text={'0'} color={Color.NEUTRAL} locked={true}></GameCard>
+				<GameCard text={game.score.toString()} color={Color.NEUTRAL} locked={true}></GameCard>
 			</FlexContainer>
 		</FlexContainer>
 	</FlexContainer>
