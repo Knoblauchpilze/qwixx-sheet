0d9b762cb4bde4e06e7ee43095bf228a2b604d90
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-01-31 20:53:09 +0100

    chore: Digit is not a class anymore

--- src/lib/game/line.test.ts
+++ src/lib/game/line.test.ts
@@ -1,10 +1,10 @@
 import { describe, expect, it } from 'vitest';
 import { DigitLine } from './line';
-import { Digit, generateAscendingLine, generateDescendingLine } from './digit';
+import { newDigit, generateAscendingLine, generateDescendingLine } from './digit';
 
 describe.concurrent('Creating line', () => {
 	it('ascending line should contain expected digits', () => {
-		const EXPECTED_DIGITS = [new Digit(1), new Digit(2)];
+		const EXPECTED_DIGITS = [newDigit(1), newDigit(2)];
 		const actual = new DigitLine(EXPECTED_DIGITS);
 
 		expect(actual.digits).toBe(EXPECTED_DIGITS);
@@ -16,7 +16,7 @@ describe.concurrent('Manipulating line', () => {
 		const actual = new DigitLine(generateAscendingLine());
 		expect(actual.score()).toBe(0);
 
-		actual.digits.at(1)?.check(true);
+		actual.digits[1].selected = true;
 		expect(actual.score()).toBe(1);
 	});
 
@@ -24,37 +24,37 @@ describe.concurrent('Manipulating line', () => {
 		const actual = new DigitLine(generateAscendingLine());
 		expect(actual.score()).toBe(0);
 
-		actual.digits.at(1)?.check(true);
+		actual.digits[1].selected = true;
 		expect(actual.score()).toBe(1);
 
-		actual.digits.at(1)?.check(true);
+		actual.digits[1].selected = true;
 		expect(actual.score()).toBe(1);
 	});
 
 	it('unticking digit should update score', () => {
 		const actual = new DigitLine(generateAscendingLine());
-		actual.digits.at(1)?.check(true);
+		actual.digits[1].selected = true;
 		expect(actual.score()).toBe(1);
 
-		actual.digits.at(1)?.check(false);
+		actual.digits[1].selected = false;
 		expect(actual.score()).toBe(0);
 	});
 
 	it('unticking already unticked digit should not update score', () => {
 		const actual = new DigitLine(generateAscendingLine());
-		actual.digits.at(1)?.check(true);
+		actual.digits[1].selected = true;
 		expect(actual.score()).toBe(1);
 
-		actual.digits.at(1)?.check(false);
+		actual.digits[1].selected = false;
 		expect(actual.score()).toBe(0);
 
-		actual.digits.at(1)?.check(false);
+		actual.digits[1].selected = false;
 		expect(actual.score()).toBe(0);
 	});
 });
 
 describe.concurrent('Manupulating digits', () => {
-	const SAMPLE_DIGITS = [new Digit(1), new Digit(2)];
+	const SAMPLE_DIGITS = [newDigit(1), newDigit(2)];
 
 	it('should disregard invalid indices', () => {
 		const line = new DigitLine(SAMPLE_DIGITS);
@@ -195,132 +195,132 @@ describe.concurrent('Calculating score', () => {
 
 	it('with 1 digit ticked', () => {
 		const actual = new DigitLine(generateAscendingLine());
-		actual.digits.at(0)?.check(true);
+		actual.digits[0].selected = true;
 
 		expect(actual.score()).toBe(1);
 	});
 
 	it('with 2 digits ticked', () => {
 		const actual = new DigitLine(generateAscendingLine());
-		actual.digits.at(0)?.check(true);
-		actual.digits.at(2)?.check(true);
+		actual.digits[0].selected = true;
+		actual.digits[2].selected = true;
 
 		expect(actual.score()).toBe(3);
 	});
 
 	it('with 3 digits ticked', () => {
 		const actual = new DigitLine(generateDescendingLine());
-		actual.digits.at(1)?.check(true);
-		actual.digits.at(3)?.check(true);
-		actual.digits.at(5)?.check(true);
+		actual.digits[1].selected = true;
+		actual.digits[3].selected = true;
+		actual.digits[5].selected = true;
 
 		expect(actual.score()).toBe(6);
 	});
 
 	it('with 4 digits ticked', () => {
 		const actual = new DigitLine(generateDescendingLine());
-		actual.digits.at(0)?.check(true);
-		actual.digits.at(1)?.check(true);
-		actual.digits.at(2)?.check(true);
-		actual.digits.at(3)?.check(true);
+		actual.digits[0].selected = true;
+		actual.digits[1].selected = true;
+		actual.digits[2].selected = true;
+		actual.digits[3].selected = true;
 
 		expect(actual.score()).toBe(10);
 	});
 
 	it('with 5 digits ticked', () => {
 		const actual = new DigitLine(generateAscendingLine());
-		actual.digits.at(2)?.check(true);
-		actual.digits.at(4)?.check(true);
-		actual.digits.at(6)?.check(true);
-		actual.digits.at(8)?.check(true);
-		actual.digits.at(10)?.check(true);
+		actual.digits[2].selected = true;
+		actual.digits[4].selected = true;
+		actual.digits[6].selected = true;
+		actual.digits[8].selected = true;
+		actual.digits[10].selected = true;
 
 		expect(actual.score()).toBe(15);
 	});
 
 	it('with 6 digits ticked', () => {
 		const actual = new DigitLine(generateAscendingLine());
-		actual.digits.at(5)?.check(true);
-		actual.digits.at(4)?.check(true);
-		actual.digits.at(3)?.check(true);
-		actual.digits.at(2)?.check(true);
-		actual.digits.at(1)?.check(true);
-		actual.digits.at(0)?.check(true);
+		actual.digits[5].selected = true;
+		actual.digits[4].selected = true;
+		actual.digits[3].selected = true;
+		actual.digits[2].selected = true;
+		actual.digits[1].selected = true;
+		actual.digits[0].selected = true;
 
 		expect(actual.score()).toBe(21);
 	});
 
 	it('with 7 digits ticked', () => {
 		const actual = new DigitLine(generateAscendingLine());
-		actual.digits.at(2)?.check(true);
-		actual.digits.at(3)?.check(true);
-		actual.digits.at(4)?.check(true);
-		actual.digits.at(5)?.check(true);
-		actual.digits.at(6)?.check(true);
-		actual.digits.at(7)?.check(true);
-		actual.digits.at(10)?.check(true);
+		actual.digits[2].selected = true;
+		actual.digits[3].selected = true;
+		actual.digits[4].selected = true;
+		actual.digits[5].selected = true;
+		actual.digits[6].selected = true;
+		actual.digits[7].selected = true;
+		actual.digits[10].selected = true;
 
 		expect(actual.score()).toBe(28);
 	});
 
 	it('with 8 digits ticked', () => {
 		const actual = new DigitLine(generateAscendingLine());
-		actual.digits.at(2)?.check(true);
-		actual.digits.at(3)?.check(true);
-		actual.digits.at(4)?.check(true);
-		actual.digits.at(5)?.check(true);
-		actual.digits.at(6)?.check(true);
-		actual.digits.at(7)?.check(true);
-		actual.digits.at(8)?.check(true);
-		actual.digits.at(10)?.check(true);
+		actual.digits[2].selected = true;
+		actual.digits[3].selected = true;
+		actual.digits[4].selected = true;
+		actual.digits[5].selected = true;
+		actual.digits[6].selected = true;
+		actual.digits[7].selected = true;
+		actual.digits[8].selected = true;
+		actual.digits[10].selected = true;
 
 		expect(actual.score()).toBe(36);
 	});
 
 	it('with 9 digits ticked', () => {
 		const actual = new DigitLine(generateAscendingLine());
-		actual.digits.at(2)?.check(true);
-		actual.digits.at(3)?.check(true);
-		actual.digits.at(4)?.check(true);
-		actual.digits.at(5)?.check(true);
-		actual.digits.at(6)?.check(true);
-		actual.digits.at(7)?.check(true);
-		actual.digits.at(8)?.check(true);
-		actual.digits.at(9)?.check(true);
-		actual.digits.at(10)?.check(true);
+		actual.digits[2].selected = true;
+		actual.digits[3].selected = true;
+		actual.digits[4].selected = true;
+		actual.digits[5].selected = true;
+		actual.digits[6].selected = true;
+		actual.digits[7].selected = true;
+		actual.digits[8].selected = true;
+		actual.digits[9].selected = true;
+		actual.digits[10].selected = true;
 
 		expect(actual.score()).toBe(45);
 	});
 
 	it('with 10 digits ticked', () => {
 		const actual = new DigitLine(generateAscendingLine());
-		actual.digits.at(1)?.check(true);
-		actual.digits.at(2)?.check(true);
-		actual.digits.at(3)?.check(true);
-		actual.digits.at(4)?.check(true);
-		actual.digits.at(5)?.check(true);
-		actual.digits.at(6)?.check(true);
-		actual.digits.at(7)?.check(true);
-		actual.digits.at(8)?.check(true);
-		actual.digits.at(9)?.check(true);
-		actual.digits.at(10)?.check(true);
+		actual.digits[1].selected = true;
+		actual.digits[2].selected = true;
+		actual.digits[3].selected = true;
+		actual.digits[4].selected = true;
+		actual.digits[5].selected = true;
+		actual.digits[6].selected = true;
+		actual.digits[7].selected = true;
+		actual.digits[8].selected = true;
+		actual.digits[9].selected = true;
+		actual.digits[10].selected = true;
 
 		expect(actual.score()).toBe(55);
 	});
 
 	it('with 11 digit ticked', () => {
 		const actual = new DigitLine(generateAscendingLine());
-		actual.digits.at(0)?.check(true);
-		actual.digits.at(1)?.check(true);
-		actual.digits.at(2)?.check(true);
-		actual.digits.at(3)?.check(true);
-		actual.digits.at(4)?.check(true);
-		actual.digits.at(5)?.check(true);
-		actual.digits.at(6)?.check(true);
-		actual.digits.at(7)?.check(true);
-		actual.digits.at(8)?.check(true);
-		actual.digits.at(9)?.check(true);
-		actual.digits.at(10)?.check(true);
+		actual.digits[0].selected = true;
+		actual.digits[1].selected = true;
+		actual.digits[2].selected = true;
+		actual.digits[3].selected = true;
+		actual.digits[4].selected = true;
+		actual.digits[5].selected = true;
+		actual.digits[6].selected = true;
+		actual.digits[7].selected = true;
+		actual.digits[8].selected = true;
+		actual.digits[9].selected = true;
+		actual.digits[10].selected = true;
 
 		expect(actual.score()).toBe(66);
 	});
