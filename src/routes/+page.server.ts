62375256e4025ebd2061ddae8080f6228756c12e
KnoblauchPilze <prolific-programmer@tuta.com>
2025-02-03 18:26:35 +0100

    feat: Implemented generate button and saved the layout and seed as cookies

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,16 @@
-import { DigitLayout } from '$lib/enums/digitLayout';
 import { generateLayout } from '$lib/game/generation';
+import type { RequestEvent } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { generateSeed } from '$lib/game/seed';
+import { loadQwixxCookies, setQwixxCookies } from '$lib/cookies';
+import { DigitLayout } from '$lib/enums/digitLayout';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ cookies }) => {
+	const [, qwixxCookies] = loadQwixxCookies(cookies);
+
 	const qwixx = {
-		layout: DigitLayout.CLASSIC,
-		seed: 'hello'
+		layout: qwixxCookies.layout,
+		seed: qwixxCookies.seed
 	};
 	const digits = generateLayout(qwixx);
 
@@ -20,4 +25,22 @@ export const load: PageServerLoad = async () => {
 
 		sameDigitBonus: 3
 	};
+};
+
+export const actions = {
+	generate: async ({ cookies, request }: RequestEvent) => {
+		const data = await request.formData();
+
+		let seed = data.get('seed');
+		if (!seed) {
+			seed = generateSeed();
+		}
+
+		const settings = {
+			layout: DigitLayout.CLASSIC,
+			seed: seed.toString()
+		};
+
+		setQwixxCookies(cookies, settings);
+	}
 };
